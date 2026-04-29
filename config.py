b4c969d7e1caac2e0d0168350ba1b01db5b80780
Rasul Yusupov <raasaal511@gmail.com>
2026-04-29 19:57:49 +0300

    Add map config for weather

--- config.py
+++ config.py
@@ -10,3 +10,14 @@ class BotSettings:
     PAYMENT_TOKEN: str = "1744374395:TEST:d2a91f796d7659a4e1c3"
 
 bot_settings = BotSettings()
+
+# bot > аддрес > координаты > api погоды > json (["fact"]["temp"]) > bot
+
+class MapConfig:
+    WEATHER_API_KEY = "52103fc8-df14-42d5-be09-40c5aface46b"
+    GEOCODE_API_KEY = "98afc61d-d922-4d9f-9040-55f99f99e2b7"
+    headers = {'X-Yandex-Weather-Key': WEATHER_API_KEY}
+    weather_url = "https://api.weather.yandex.ru/v2/forecast"
+    geocode_url = f"https://geocode-maps.yandex.ru/1.x/?apikey={GEOCODE_API_KEY}"
+
+map_config = MapConfig()
