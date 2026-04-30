0b2b95e432a0e9f4772d54c1f918801933c69ee4
Rasul Yusupov <raasaal511@gmail.com>
2026-04-30 19:36:03 +0300

    work

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,10 +1,10 @@
 from aiogram import Router
 
-# from routers.start import router as start_router
-# from routers.specials import router as special_router
-# from routers.handle_callback import router as callback_router
-# from routers.group_games import router as game_router
-# from routers.keywords import router as keyword_router
+from routers.start import router as start_router
+from routers.specials import router as special_router
+from routers.handle_callback import router as callback_router
+from routers.group_games import router as game_router
+from routers.keywords import router as keyword_router
 from routers.chat_member import router as chat_router
 from routers.auth import router as auth_router
 from routers.inline_movies import router as inline_router
@@ -18,11 +18,11 @@ router = Router()
 # start_router.message.middleware(SlowMiddleware(delay=5))
 
 router.include_routers(
-    # start_router,
-    # special_router,
-    # callback_router,
-    # game_router,
-    # keyword_router,
+    start_router,
+    special_router,
+    callback_router,
+    game_router,
+    keyword_router,
     inline_router,
     movie_router,
     chat_router,
